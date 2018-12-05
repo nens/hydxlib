73aa8a254b443ac69066b4b2e755e38c88950eda
Reinout van Rees <reinout@vanrees.org>
2018-12-05 17:41:42 +0100

    Made filename plural

--- gwswlib/tests/test_scripts.py
+++ gwswlib/tests/test_scripts.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """Tests for script.py"""
-from gwswlib import script
+from gwswlib import scripts
 
 
 def test_get_parser():
-    parser = script.get_parser()
+    parser = scripts.get_parser()
     # As a test, we just check one option. That's enough.
     options = parser.parse_args()
     assert options.verbose is False
