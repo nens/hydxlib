144e9a6edd091e002facfddc093edff0c36c2a8a
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2022-11-16 14:01:30 +0100

    Update threedi.py

--- hydxlib/threedi.py
+++ hydxlib/threedi.py
@@ -73,6 +73,8 @@ SEWERAGE_TYPE_MAPPING = {
 MANHOLE_INDICATOR_MAPPING = {
     "INS": ManholeIndicator.MANHOLE.value,
     "UIT": ManholeIndicator.OUTLET.value,
+    "ITP": ManholeIndicator.MANHOLE.value,
+    "CMP": ManholeIndicator.MANHOLE.value,
 }
 
 # for now skipping, "HEU"
