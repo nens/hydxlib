47431763a4596159e2b049cf4dbcfee7a5bcbf0f
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2025-03-10 16:33:12 +0100

    Clean up to make linter happy

--- hydxlib/tests/test_exporter.py
+++ hydxlib/tests/test_exporter.py
@@ -1,46 +1,38 @@
 # -*- coding: utf-8 -*-
 """Tests for importer.py"""
-import json
+
 import pytest
-from hydxlib.exporter import (
-    export_threedi,
-    get_cross_section_fields,
-    get_connection_node,
-    get_start_and_end_connection_node,
-    get_cross_section_fields,
-    write_threedi_to_db,
-    get_line_between_nodes,
-    get_node_geom,
-)
-from hydxlib.threedi import Threedi
+from geoalchemy2.shape import from_shape
+from shapely import wkt
 from threedi_schema import models
 
-from shapely import wkt
-from geoalchemy2.shape import from_shape
+from hydxlib.exporter import (
+    export_threedi,
+    get_connection_node,
+    get_cross_section_fields,
+    get_line_between_nodes,
+    get_node_geom,
+    get_start_and_end_connection_node,
+    write_threedi_to_db,
+)
+from hydxlib.threedi import Threedi
 
 
 class TestGetCrossSectionFields:
-
     def test_get_cross_section_fields_not_tabulated(self):
         connection = {"code": "conn1", "cross_section_code": "cs1"}
-        cross_section_dict = {
-            "cs1": {"shape": 1, "width": "4.2", "height": "2.1"}
-        }
+        cross_section_dict = {"cs1": {"shape": 1, "width": "4.2", "height": "2.1"}}
         updated_connection = get_cross_section_fields(connection, cross_section_dict)
         assert updated_connection["cross_section_shape"] == 1
         assert updated_connection["cross_section_width"] == "4.2"
         assert updated_connection["cross_section_height"] == "2.1"
 
-
     def test_get_cross_section_fields_with_missing_definition(self, caplog):
         connection = {"code": "conn2", "cross_section_code": "missing_cs"}
-        cross_section_dict = {
-            "cs1": {"shape": 1, "width": "4.2", "height": "2.1"}
-        }
+        cross_section_dict = {"cs1": {"shape": 1, "width": "4.2", "height": "2.1"}}
         updated_connection = get_cross_section_fields(connection, cross_section_dict)
         assert updated_connection == connection
         assert caplog.records[0].levelname == "ERROR"
-
 
     def test_get_cross_section_fields_tabulated_YZ(self):
         connection = {"code": "conn3", "cross_section_code": "cs_tabulated"}
@@ -48,7 +40,7 @@ class TestGetCrossSectionFields:
             "cs_tabulated": {
                 "shape": 7,
                 "width": "1.1 2.2 3.3",
-                "height": "0.1 0.2 0.3"
+                "height": "0.1 0.2 0.3",
             }
         }
         updated_connection = get_cross_section_fields(connection, cross_section_dict)
@@ -61,7 +53,7 @@ class TestGetCrossSectionFields:
             "cs_tabulated": {
                 "shape": 6,
                 "width": "0.1 0.2 0.3",
-                "height": "1.1 2.2 3.3"
+                "height": "1.1 2.2 3.3",
             }
         }
         updated_connection = get_cross_section_fields(connection, cross_section_dict)
@@ -87,15 +79,19 @@ def test_get_line_between_nodes():
     geom2 = from_shape(wkt.loads("POINT (400 60)"), srid=28992)
     connection = {"code": "pmp1", "nodeA.code": "knp3", "nodeB.code": "knp4"}
     connection_node_dict = {"knp3": {"geom": geom1}, "knp4": {"geom": geom2}}
-    line = get_line_between_nodes(connection, connection_node_dict, "nodeA.code", "nodeB.code")
-    assert line['geom'] == 'SRID=28992;LINESTRING (400.0 50.0, 400.0 60.0)'
+    line = get_line_between_nodes(
+        connection, connection_node_dict, "nodeA.code", "nodeB.code"
+    )
+    assert line["geom"] == "SRID=28992;LINESTRING (400.0 50.0, 400.0 60.0)"
 
 
 def test_get_line_between_nodes_incomplete(caplog):
     connection = {"code": "pmp1", "nodeA.code": "knp3", "nodeB.code": "knp4"}
     connection_node_dict = {"knp3": {"geom": "foo"}}
-    line = get_line_between_nodes(connection, connection_node_dict, "nodeA.code", "nodeB.code")
-    assert line['geom'] is None
+    line = get_line_between_nodes(
+        connection, connection_node_dict, "nodeA.code", "nodeB.code"
+    )
+    assert line["geom"] is None
     assert caplog.records[0].levelname == "ERROR"
 
 
@@ -118,9 +114,6 @@ def test_get_start_and_end_connection_node():
     connection = get_start_and_end_connection_node(connection, connection_node_dict)
     assert connection["connection_node_id_start"] == 3060
     assert connection["connection_node_id_end"] == 3061
-
-
-#TODO: add unit tests
 
 
 @pytest.fixture
@@ -168,4 +161,3 @@ def test_write_to_db(hydx_setup, mock_exporter_db, threedi_db):
     }
     for name, model in MODELS.items():
         assert session.query(model).count() == commit_counts_expected[name]
-
